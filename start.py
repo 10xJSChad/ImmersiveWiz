78b9d0f7b22e708b779b475d9c515f6924cb1f0a
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-06-29 02:31:45 +0200

    Added a color multiplier

--- start.py
+++ start.py
@@ -26,10 +26,18 @@ async def allOff(): #Self explanatory
     for bulb in bulbs:
         await bulb.turn_off()
   
+def intensifyColor(color, multiplier):
+    newColors = []
+    for x in color:
+        x *= multiplier
+        if x > 255: x = 255
+        newColors.append(x)
+    color = (newColors[0], newColors[1], newColors[2])
+    return color
+
 async def main():  
     while True:
         if (GetWindowText(GetForegroundWindow()) == config.window or config.window == ""): #Self explanatory
-            
             #Takes a screenshot of your main screen
             screenshot = pyautogui.screenshot()
             screenshot.save(r'screenshot.png') #Saves it to the working folder
@@ -38,7 +46,7 @@ async def main():
             screenshot = cv2.imread('screenshot.png')
             avg_color_per_row = numpy.average(screenshot, axis=0)
             avg_color = numpy.average(avg_color_per_row, axis=0)
-            
+            avg_color_fixed = (int(avg_color[2]), int(avg_color[1]), int(avg_color[0]))
             
             #IMPORTANT
             #avg_color[0] is blue
@@ -48,7 +56,7 @@ async def main():
             #Gets the total value of the red, green & blue values of the average color
             totalValue = int(avg_color[2]) + int(avg_color[1]) + int(avg_color[0])
             if totalValue > config.lightsOffThreshold: #lightsOffThreshold is the value at which the lights turn off
-                await changeAll((int(avg_color[2]), int(avg_color[1]), int(avg_color[0])))    
+                await changeAll(intensifyColor(avg_color_fixed, config.colorMultiplier))    
             else: await allOff() #Turns the lights off if the total value is less than lightsOffThreshold
             
             #print(str(avg_color) + " | " + str(totalValue))
