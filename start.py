0de80a41aa3a620952d768d5b8b4aab9a08c3c59
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-06-29 12:34:42 +0200

    Added the option to use dominant color
    And changed some variable names

--- start.py
+++ start.py
@@ -16,15 +16,20 @@ async def initialize():
     global bulbs
     bulbs = await discovery.discover_lights(broadcast_space=config.broadcastSpace)
 
-async def changeAll(color): #Self explanatory
+async def changeAll(color):
     global bulbs
     for bulb in bulbs:
         await bulb.turn_on(PilotBuilder(rgb = (color)))
         
-async def allOff(): #Self explanatory
+async def allOff():
     global bulbs
     for bulb in bulbs:
         await bulb.turn_off()
+        
+async def setBrightness(newBrightness): 
+    global bulbs
+    for bulb in bulbs:
+        await bulb.turn_on(PilotBuilder(brightness = newBrightness))
   
 def intensifyColor(color, multiplier):
     newColors = []
@@ -35,31 +40,50 @@ def intensifyColor(color, multiplier):
     color = (newColors[0], newColors[1], newColors[2])
     return color
 
+def getAverageColor(screenshot):
+        avg_color_per_row = numpy.average(screenshot, axis=0)
+        avg_color = numpy.average(avg_color_per_row, axis=0)
+        return avg_color
+
+def getDominantColor(screenshot):
+    colors, count = numpy.unique(screenshot.reshape(-1,screenshot.shape[-1]), axis=0, return_counts=True)
+    return colors[count.argmax()]
+            
 async def main():  
     while True:
-        if (GetWindowText(GetForegroundWindow()) == config.window or config.window == ""): #Self explanatory
+        if (GetWindowText(GetForegroundWindow()) == config.window or config.window == ""):
+            
             #Takes a screenshot of your main screen
             screenshot = pyautogui.screenshot()
             screenshot.save(r'screenshot.png') #Saves it to the working folder
             
-            #Gets the average color
             screenshot = cv2.imread('screenshot.png')
-            avg_color_per_row = numpy.average(screenshot, axis=0)
-            avg_color = numpy.average(avg_color_per_row, axis=0)
-            avg_color_fixed = (int(avg_color[2]), int(avg_color[1]), int(avg_color[0]))
+            
+            #Gets the average or dominant color
+            if config.colorToUse == "dominant": 
+                screenColor = getDominantColor(screenshot)
+            else: screenColor = getAverageColor(screenshot)
+            
+            #Formats it for the bulbs 
+            screenColorFixed = (int(screenColor[2]), int(screenColor[1]), int(screenColor[0]))
             
             #IMPORTANT
-            #avg_color[0] is blue
-            #avg_color[1] is green
-            #avg_color[2] is red
+            #screenColor[0] is blue
+            #screenColor[1] is green
+            #screenColor[2] is red
+            
+            #screenColorFixed[0] is red
+            #screenColorFixed[1] is green
+            #screenColorFixed[2] is blue
             
             #Gets the total value of the red, green & blue values of the average color
-            totalValue = int(avg_color[2]) + int(avg_color[1]) + int(avg_color[0])
+            totalValue = screenColorFixed[0] + screenColorFixed[1] + screenColorFixed[2]
             if totalValue > config.lightsOffThreshold: #lightsOffThreshold is the value at which the lights turn off
-                await changeAll(intensifyColor(avg_color_fixed, config.colorMultiplier))    
+                await changeAll(intensifyColor(screenColorFixed, config.colorMultiplier))  
+                await setBrightness(config.bulbBrightness)
             else: await allOff() #Turns the lights off if the total value is less than lightsOffThreshold
             
-            #print(str(avg_color) + " | " + str(totalValue))
+            #print(str(screenColorFixed) + " | " + str(totalValue))
             
         else: 1+1
         
